a8d0f47083b9ad03dbef1117f4d8d22d0530d483
proneasacatepequez-max <proneasacatepequez@gmail.com>
2026-04-17 13:30:38 -0600

    Add remote pattern for api.qrserver.com

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: '**.googleusercontent.com' },
+      { protocol: 'https', hostname: 'api.qrserver.com' },
     ],
   },
 }
